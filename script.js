ba5c5310803cfbef692aab92c874873d8d3e88de
mdada12 <60120498+mdada12@users.noreply.github.com>
2020-01-29 23:44:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Declare variables 
+    Scanner scanner = new Scanner( System. in );
+double mealCost; 
+double tax;
+double totalCost; 
+double tip
+double totalBill;
+
+System.out.println( "Plaese enter the total of the bill: " );
+scanner.nextDouble();
+
+//calculate the tax
+tax = 0.05 * mealCost;
+
+totalCost = mealCost + tax;
+
+tip = 0.07 * totalCost;
+
+totalBill = totalCost + tip
+
+totalBill = totalCost + tip
+System.out.println( "Cost of meal: $" + mealCost);
+System.out.println( "Tax: $" + tax );
+System.out.println( "Tip: " + tip );
+System.out.println( "Total Bill: " + totalBill );
